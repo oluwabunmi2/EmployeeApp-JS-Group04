3e7c26ff45532f2ad885c221dde0ce3adfea915f
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-15 21:34:03 +0200

    first implement

---
+++ EmployeeApp.js
@@ -0,0 +1,25 @@
+class Employee{
+
+    constructor(){
+        id= Number;
+        Name = String;
+        department= String;
+        salary= Number;
+    }
+
+    getID(){
+        return this.id;
+    }
+
+    getname(){
+        return this.Name;
+    }
+
+    getdepartment(){
+        return this.department;
+    }
+
+    getsalary(){
+        return this.salary;
+    }
+}
